c902134140b75bdaf29647c79b39246e6e6d6adc
James Dickinson <jimmy@jimmyinteractive.com>
2018-06-18 15:22:58 -0700

    update Google Modile Ad SDK for Admob to 7.31.0

--- src/ios/GoogleMobileAds.framework/Headers/GADVideoController.h
+++ src/ios/GoogleMobileAds.framework/Headers/GADVideoController.h
@@ -17,8 +17,7 @@ GAD_ASSUME_NONNULL_BEGIN
 /// The video controller class provides a way to get the video metadata and also manages video
 /// content of the ad rendered by the Google Mobile Ads SDK. You don't need to create an instance of
 /// this class. When the ad rendered by the Google Mobile Ads SDK loads video content, you may be
-/// able to get an instance of this class from the rendered ad object. Currently only native express
-/// ad view class exposes video controller.
+/// able to get an instance of this class from the rendered ad object.
 @interface GADVideoController : NSObject
 
 /// Delegate for receiving video notifications.
@@ -42,6 +41,8 @@ GAD_ASSUME_NONNULL_BEGIN
 /// Indicates if video custom controls (i.e. play/pause/mute/unmute) are enabled.
 - (BOOL)customControlsEnabled;
 
+/// Indicates if video click to expand behavior is enabled.
+- (BOOL)clickToExpandEnabled;
 @end
 
 GAD_ASSUME_NONNULL_END
--- src/ios/GoogleMobileAds.framework/Headers/Mediation/GADMAdNetworkConnectorProtocol.h
+++ src/ios/GoogleMobileAds.framework/Headers/Mediation/GADMAdNetworkConnectorProtocol.h
@@ -24,6 +24,12 @@
 /// of the returned UIViewController .
 - (UIViewController *)viewControllerForPresentingModalView;
 
+/// Returns the preferred ad volume as a fraction of system volume (0.0 to 1.0).
+- (float)adVolume;
+
+/// Returns whether the ad should be muted.
+- (BOOL)adMuted;
+
 #pragma mark - Adapter Callbacks
 
 /// Tells the connector that the adapter failed to receive an ad.
@@ -36,9 +42,15 @@
 - (void)adapterDidReceiveInterstitial:(id<GADMAdNetworkAdapter>)adapter;
 
 /// Tells the connector that the adapter has received a mediated native ad. |mediatedNativeAd| is
-/// used by the Google Mobile Ads SDK for constructing a native ad object.
+/// used by the Google Mobile Ads SDK to construct a native ad object.
 - (void)adapter:(id<GADMAdNetworkAdapter>)adapter
     didReceiveMediatedNativeAd:(id<GADMediatedNativeAd>)mediatedNativeAd;
+
+/// Tells the connector that the adapter has received a unified mediated native ad.
+/// mediatedUnifiedNativeAd is used by the Google Mobile Ads SDK to construct a unified native ad
+/// object.
+- (void)adapter:(id<GADMAdNetworkAdapter>)adapter
+    didReceiveMediatedUnifiedNativeAd:(id<GADMediatedUnifiedNativeAd>)mediatedUnifiedNativeAd;
 
 #pragma mark Ad events
 
